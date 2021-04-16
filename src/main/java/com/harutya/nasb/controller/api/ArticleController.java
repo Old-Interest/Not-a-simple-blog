55f00616cce2ba2064180a70ec0f4856249a982d
unknown <thouartmyhope@foxmail.com>
2021-04-16 18:14:02 +0800

    test jenkins

--- src/main/java/com/harutya/nasb/controller/api/ArticleController.java
+++ src/main/java/com/harutya/nasb/controller/api/ArticleController.java
@@ -20,6 +20,6 @@ public class ArticleController {
     @GetMapping
     @ApiOperation(value = "get all articles",httpMethod = "GET")
     public BaseResponse<String> article(){
-        return BaseResponse.success("Hello World","szg nmmmdl!");
+        return BaseResponse.success("Hello World","szg nmmmdl!!");
     }
 }
